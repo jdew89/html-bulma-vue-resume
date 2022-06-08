848bcc87fd780a96177e3d9054f69726fa6c68ff
Jeff DeWitt <jadewitt00@gmail.com>
2022-06-08 17:03:17 -0400

    set up first vue component

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,56 @@
 import { createApp } from 'vue'
 
-  createApp({
-    data() {
-      return {
-        message: 'Hello Vue!'
-      }
+const MyComp = {
+  template: '#my-comp-template'
+  // ...
+}
+
+const app = createApp({
+  data() {
+    return {
+      message: 'Hello Vue!',
+      skills: [
+        "Windows Administration",
+        "Server Administration",
+        "Docker",
+        "AWS",
+        "Powershell",
+        "G Suite Administration",
+        "Javascript", 
+        "Golang",
+        "Python",
+        "Oracle",
+        "SQL Server Management",
+        "Customer Service"
+      ]
     }
-  }).mount('#resume')
+  }
+});
+
+console.log(document.getElementById("section-title-template").innerHTML);
+
+app.component(
+  // the registered name
+  'SectionTitle',
+  // the implementation
+  {
+    
+    template: document.getElementById("section-title-template").innerHTML,
+    //template: "#section-title-template",
+    data(){
+      return {
+        
+      }
+    },
+    props: {
+      sectiontitle: String
+    }
+  }
+);
+
+app.mount('#resume');
+
+
+
+
+  
