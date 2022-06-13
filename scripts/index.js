797e07d18512c2be51cd5d868d480cc5cf83e156
Jeff DeWitt <jadewitt00@gmail.com>
2022-06-13 16:59:07 -0400

    finished formatting

--- scripts/index.js
+++ scripts/index.js
@@ -12,13 +12,13 @@ const app = createApp({
       location: "Canton,OH",
       phone: "(440) 539-1862",
       email: "jadewitt00@gmail.com",
-      skills: [
+      skills_old: [
         "Windows Server Administration",
         //"Server Administration",
         "Docker",
         "Powershell",
         //"G Suite Administration",
-        "Javascript",
+        //"Javascript",
         "NodeJS",
         "Golang",
         //"Python",
@@ -26,12 +26,20 @@ const app = createApp({
         //"SQL Server Management",
         //"Customer Service"
       ],
-      certs: ["AWS Certified Cloud Practitioner (2021)","AWS Certified Solutions Architect (2021)"],
+      skills: {
+        Programming: ["Powershell", "Javascript", "NodeJS", "GoLang", "Python", "SQL"],
+        Software: ["Docker", "GitHub", "AWS"],
+        Databases: ["PostgreSQL", "SQL Server", "Oracle DB"],
+        Administration: ["SQL Server", "Oracle DB", "Windows Server", "Linux (Ubuntu, Amazon)"],
+        //Cloud: ["AWS"],
+      },
+      certs: ["AWS Certified Cloud Practitioner (May 2021)","AWS Certified Solutions Architect (Dec 2021)"],
       education: {
         degree: "Bachelor of Science",
         field: "Computer Science",
         gradyear: 2013,
-        school: "Malone University"
+        school: "Malone University",
+        location: "Canton, OH"
       },
       // excellent customer service and
       summary: `Talented Information Technology Professional with 7+ years of Windows administration experience and certified in AWS cloud architecture.
@@ -42,74 +50,77 @@ const app = createApp({
           jobtitle: "Software Application Engineer",
           daterange: "10/2021 to Present",
           company: "3E Company",
+          location: "Canton, OH",
           tasks: [
-            'Created automated windows deployments using powershell.',
-            'Configured application to run on AWS Appstream and RDS.',
-            'Containerized our .NET api\'s in docker and deployed on AWS.',
-            'Troubleshoot and support our enterprise Windows desktop application.',
-            'Configure and install our enterprise Windows desktop application.',
-            'Administer IIS .NET applications for customers.',
-            'Migrated our hosted application from on-site to AWS.',
-            'Work with high profile customers to help troubleshoot on-prem environment installations.'
+            'Created automated windows deployments using powershell',
+            'Configured legacy Windows application to run on AWS Appstream and RDS',
+            'Containerized our .NET api\'s in docker and deployed on AWS',
+            'Supported our enterprise Windows desktop application',
+            'Administered IIS .NET applications for customers',
+            'Migrated our hosted application from on-site to AWS',
+            'Worked with high profile customers to help troubleshoot on-prem environment installations'
           ]
         },
         {
           jobtitle: "Full Stack Engineer",
           daterange: "10/2018 to Present",
           company: "Easy Earnest",
+          location: "Canton, OH",
           tasks: [
-            'Designed and built SaaS product for making electronic earnest money payments.',
-            'Coded in Node and Express for backend.',
-            'Front end coded in VueJS.',
-            'Data stored using PostgreSQL.',
-            'Hosted and maintained on AWS infrastructure.',
+            'Designed and built SaaS product for making electronic earnest money payments',
+            'Coded in Node and Express for backend and VueJs for frontend',
+            'Stored data using PostgreSQL',
+            'Hosted and maintained on AWS infrastructure',
           ]
         },
         {
           jobtitle: "IT Director",
           daterange: "10/2018 to 03/2021",
           company: "Cutler Real Estate",
+          location: "Canton, OH",
           tasks: [
-            'Provide technical support for 400+ agents and staff.',
-            'Converted a fully physical server environment to a virtual environment.',
-            'Use Connectwise Control to remotely support 20+ offices.',
-            'Manage 80+ domain users through Active Directory and Group Policy.',
-            'Perform maintenance and deploy computers for staff.',
-            'Deployed MDT server for more efficient desktop deployment.',
-            'Track inventory of all hardware and software licenses.',
-            'Work with outside vendors to provide business needs.',
-            'Manage the IT budget and resource allocation.'
+            'Provided technical support for 400+ agents and staff',
+            'Converted a fully physical server environment to a virtual environment',
+            //'Used Connectwise Control to remotely support 20+ offices',
+            'Managed 80+ domain users through Active Directory and Group Policy',
+            'Performed maintenance and deploy computers for staff',
+            'Deployed MDT server for more efficient desktop deployment',
+            'Tracked inventory of all hardware and software licenses',
+            'Worked with outside vendors to provide business needs',
+            'Managed the IT budget and resource allocation'
           ]
         },
         {
           jobtitle: "IT Support Specialist",
           daterange: "03/2017 to 10/2018",
           company: "Cutler Real Estate",
+          location: "Canton, OH",
           tasks: [
-            'Provided technical support for 400+ agents and staff.',
-            'Used LogMeIn Rescue to remotely support 20+ offices.',
-            'Designed and deployed Happyfox ticketing system.',
-            'Facilitated a conversion from Delta Mail to the G Suite product.',
-            'Manage 80+ domain users through Active Directory and Group Policy.',
-            'Complete computer installs for staff computers.',
-            'Track inventory of all hardware and software licenses.',
+            'Provided technical support for 400+ agents and staff',
+            'Used LogMeIn Rescue to remotely support 20+ offices',
+            'Designed and deployed Happyfox ticketing system',
+            'Facilitated a conversion from Delta Mail to the G Suite product',
+            'Managed 80+ domain users through Active Directory and Group Policy',
+            'Completed computer installs for staff computers',
+            'Tracked inventory of all hardware and software licenses',
           ]
-        },
+        },/*
         {
           jobtitle: "IT Customer Support Specialist",
           daterange: "02/2016 to 02/2017",
           company: "Insight for Living",
+          location: "Frisco, TX",
           tasks: [
-            'Provided technical, operation and training support for users.',
-            'Completed computer refreshes for client computers.',
-            'Tracked inventory of all hardware and software licenses.',
-            'Ensured printers are working properly and performed basic maintenance.',
-            'Assisted with bill coding and data entry.',
-            'Created documentation for procedures and installs.',
-            'Edited Active Directory and GPOs.',
-            'Patched, tested, and installed Ethernet cable.',
+            'Provided technical, operation and training support for users',
+            'Completed computer refreshes for client computers',
+            'Tracked inventory of all hardware and software licenses',
+            'Ensured printers are working properly and performed basic maintenance',
+            'Assisted with bill coding and data entry',
+            'Created documentation for procedures and installs',
+            'Edited Active Directory and GPOs',
+            'Patched, tested, and installed Ethernet cable',
           ]
-        },
+        },*/
       ],
     }
   }
@@ -149,6 +160,7 @@ app.component(
       jobtitle: String,
       daterange: String,
       company: String,
+      location: String,
       tasks: Array
     }
   }
